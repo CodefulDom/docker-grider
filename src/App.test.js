4a7523c70ca5167a133f02889077e4cb0ba90233
codefuldom <dom@codefuldom.com>
2020-01-23 14:01:05 -0600

    :rocket: complete section 6

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 describe('first mother fucking test bitch', () => {
   it('should render without falling the fuck over', () => {})
 })
+
 describe('second mother fucking test bitch', () => {
   it('should render without falling the fuck over', () => {})
 })
